6452060ebc873a887ea0da57615cd1a07f33a6d2
Benadav <Benadav123@gmail.com>
2022-11-13 20:43:47 +0200

    This is exercise 3

--- bennadav.java
+++ bennadav.java
@@ -1,7 +1,7 @@
 public class bennadav{
 	public static void main(String[] args){
 		for(int i=1; i<=10; i++){
-			System.out.println("bennadav..."+i);
+			System.out.println("different bennadav..."+i);
 		}
 	}
 }
